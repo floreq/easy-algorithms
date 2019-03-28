53cd2edd65e75d7bf45cff4961cec112799d3f54
Piotr Florczak <piotrflorczakk@gmail.com>
2019-03-28 21:30:45 +0100

    Nowa dzialka

---
+++ nowa-dzialka/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int D, X;
+
+int main()
+{
+    cin >> D;
+
+    for (int i=0; i<D; i++) {
+        cin >> X;
+        cout << X * X << endl;    
+    };
+
+    return 0;
+}
